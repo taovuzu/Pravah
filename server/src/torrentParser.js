17e009cc8dd811a81ebe8aeb7d71e99f42b77172
taovuzu <yatindanger0001@gmail.com>
2025-07-07 16:21:27 +0530

    add infoHash and size function to torrentParser

--- server/src/torrentParser.js
+++ server/src/torrentParser.js
@@ -1,15 +1,19 @@
 import * as fs from 'fs/promises';
 import bencode from 'bencode';
+import crypto from 'crypto';
 
 export const open = async (filePath) => {
   const buffer = await fs.readFile(filePath);
   return bencode.decode(buffer, 'utf8');
 }
 
-export const size = () => {
-
+export const size = (torrent) => {
+  const size = torrent.info.files ? torrent.info.files.map(file => BigInt(file.length)).reduce((a, b) => a + b) : BigInt(torrent.info.length);
+  const buf = Buffer.allocUnsafe(8);
+  return buf.writeBigUInt64BE(size);
 }
 
-export const infoHash = () => {
-  
+export const infoHash = (torrent) => {
+  const info = bencode.encode(torrent.info);
+  return crypto.createHash('sha1').update(info).digest();
 }
